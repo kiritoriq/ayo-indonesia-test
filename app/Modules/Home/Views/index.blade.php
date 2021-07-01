a53a3e53f8ce6495fdc42651616f2612a3ebfcbe
unknown <thoriqmuller13@gmail.com>
2021-07-02 01:09:30 +0700

    add data psc119 dashboard

--- app/Modules/Home/Views/index.blade.php
+++ app/Modules/Home/Views/index.blade.php
@@ -51,6 +51,12 @@
                                     </a>
                                 </li>
                                 <li class="nav-item">
+                                    <a class="nav-link" data-toggle="tab" href="#data-psc119">
+                                        <span class="nav-icon"><i class="fas fa-hospital-alt"></i></span>
+                                        <span class="nav-text font-13 font-weight-bold">Data Public Safety Center (PSC) 119</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
                                     <a class="nav-link" data-toggle="tab" href="#data-puskesmas">
                                         <span class="nav-icon"><i class="far fa-hospital"></i></span>
                                         <span class="nav-text font-13 font-weight-bold">Puskesmas Jawa Tengah</span>
@@ -99,6 +105,47 @@
                                                 <th>Kelurahan</th>
                                             </tr>
                                         </thead>
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade show" id="data-psc119" role="tabpanel" aria-labelledby="data-psc119">
+                                <div class="table-responsive">
+                                    <div class="form-group row fv-plugins-icon-container">
+                                        <div class="col-lg-12 col-12">
+                                            <div class="input-group">
+                                                <input type="text" id="cariDatapsc" class="form-control h-auto" placeholder="Cari PSC 119"
+                                                    name="namaRS">
+                                                <div class="input-group-append">
+                                                    <span class="input-group-text"><span class="svg-icon svg-icon-success svg-icon-2x"><!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-05-14-112058/theme/html/demo1/dist/../src/media/svg/icons/General/Search.svg--><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                                                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                            <rect x="0" y="0" width="24" height="24"/>
+                                                            <path d="M14.2928932,16.7071068 C13.9023689,16.3165825 13.9023689,15.6834175 14.2928932,15.2928932 C14.6834175,14.9023689 15.3165825,14.9023689 15.7071068,15.2928932 L19.7071068,19.2928932 C20.0976311,19.6834175 20.0976311,20.3165825 19.7071068,20.7071068 C19.3165825,21.0976311 18.6834175,21.0976311 18.2928932,20.7071068 L14.2928932,16.7071068 Z" fill="#000000" fill-rule="nonzero" opacity="0.3"/>
+                                                            <path d="M11,16 C13.7614237,16 16,13.7614237 16,11 C16,8.23857625 13.7614237,6 11,6 C8.23857625,6 6,8.23857625 6,11 C6,13.7614237 8.23857625,16 11,16 Z M11,18 C7.13400675,18 4,14.8659932 4,11 C4,7.13400675 7.13400675,4 11,4 C14.8659932,4 18,7.13400675 18,11 C18,14.8659932 14.8659932,18 11,18 Z" fill="#000000" fill-rule="nonzero"/>
+                                                        </g>
+                                                    </svg><!--end::Svg Icon--></span></span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <table class="table table-bordered table-hover table-checkable" id="datatable-psc">
+                                        <thead>
+                                            <tr>
+                                                <th width="5%">No</th>
+                                                <th>Nama</th>
+                                                <th>Alamat</th>
+                                                <th>Nomor Telepon</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach($data_psc as $row => $item)
+                                            <tr>
+                                                <td class="text-center" width="5%">{!! $row++ !!}</td>
+                                                <td>{{ $item->nama_psc }}</td>
+                                                <td>{{ $item->alamat }}</td>
+                                                <td>{{ $item->telepon }}</td>
+                                            </tr>
+                                            @endforeach
+                                        </tbody>
                                     </table>
                                 </div>
                             </div>
@@ -460,7 +507,47 @@
 
         dataTablePuskesmas.init()
 
+        const KTDatatablesBasicBasic = function () {
+            const dataTableInit = function dataTableInit() {
+                    let table = $('#datatable-psc').DataTable({
+                    responsive: true,
+                    dom: "<'row'<'col-sm-12'tr>>\n\t\t\t<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>",
+                    lengthMenu: [5, 10, 25, 50],
+                    pageLength: 10,
+                    info: false,
+                    ordering: false,
+                    language: {
+                        'lengthMenu': 'Display _MENU_'
+                    },
+                    columnDefs: []
+                    });
+
+                table.on('order.dt search.dt', function () {
+                    table.column(0, {search: 'applied', order: 'applied'}).nodes().each(function (cell, i) {
+                        cell.innerHTML = i + 1;
+                    });
+                }).draw();
+
+                $("#cariDatapsc").on("keyup", function (e) {
+                    if ($(this).val() === "") {
+                        table.search($("#cariDatapsc").val()).draw();
+                    } else {
+                        table.columns(1).search($("#cariDatapsc").val()).draw();
+                    }
+                });
+
+            };
+
+            
+            return {
+                init: function init() {
+                dataTableInit();
+                }
+            };
+            }();
+
         $(document).ready(function() {
+            KTDatatablesBasicBasic.init();
             var chart = echarts.init(document.getElementById('chart'));
             var option = {
                 title: {
@@ -495,6 +582,10 @@
                         itemStyle: {
                             color: 'rgba(0, 170, 19, 1)'
                         },
+                        label: {
+                            show: true,
+                            position: 'top'
+                        }
                     }
                     $.each(laporan, function(key, value) {
                         var items = value.split(',')
