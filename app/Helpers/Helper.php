941263e5b8415b3b3828d81dd137c675151128b4
unknown <thoriqmuller13@gmail.com>
2021-07-07 23:00:01 +0700

    tambah route cetak excel vaksinasi

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -456,4 +456,15 @@ class Helper
         return $output;
     }
 
+    public static function formatTanggalPanjang($tanggal) {
+        if(substr($tanggal, 0,9) != '00-00-000' || substr($tanggal, 0,9) != ''){
+            $aBulan = array(1=> "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
+            list($thn,$bln,$tgl)=explode("-",$tanggal);
+            $bln = (($bln >0 ) && ($bln < 10))? substr($bln,1,1): $bln ;
+            return $tgl." ".$aBulan[$bln]." ".$thn;
+        }else{
+            return '';
+        }
+    }
+
 }
